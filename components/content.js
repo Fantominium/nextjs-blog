b66c9f6e45f468c28257723c440809fcefb00efe
malcolm <Malcom@Malcolms-MacBook-Pro.local>
2021-03-30 16:49:09 +0100

    dynamic routes completed with the id file in the pages/posts directory

--- components/content.js
+++ components/content.js
@@ -2,14 +2,32 @@ import innerContent from './style_modules/content.module.css'
 import utilStyles from '../styles/utils.module.css'
 
 
-export default function Content(){
+export default function Content(props){
     return(
-        <section className={utilStyles.headingMd + ' ' + innerContent.container} >
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+      <>
+          <section className={utilStyles.headingMd + ' ' + innerContent.container} >
+          <p>[Your Self Introduction]</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this on{' '}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {props.allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
+
+      </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,43 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Content from '../components/content'
-export default function Home() {
+
+import { getSortedPostsData } from '../lib/posts'
+
+import useSWR from 'swr'
+
+
+//personal test of the swr facility for client side data fetching for user specific pages ideally
+export function callSwapiPlanets () {
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error } = useSWR('https://swapi.dev/api/planets/1/', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  return <div>Hello from {data.name}!</div>
+}
+
+//run server side, ideal for pre-rendering static components from a db 
+//use this for the "Places worked" or "Projects"
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
+      {callSwapiPlanets()}
       <Head>
         <title>{siteTitle}</title>
       </Head>
-        <Content/>
+        <Content allPostsData={allPostsData}/>
     </Layout>
   )
 }
