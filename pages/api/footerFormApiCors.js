3e950be4a80dd0f73b1c648ec0678ff4dc77b830
malcolm <Malcom@Malcolms-MacBook-Pro.local>
2021-04-09 17:21:57 +0100

    send grid email working. Using my api key which is not included for obv reasons

--- pages/api/footerFormApiCors.js
+++ pages/api/footerFormApiCors.js
@@ -1,5 +1,9 @@
 import Cors from 'cors'
 import initLogicCors from "../../lib/initLogicCors"
+import sgMail from '@sendgrid/mail'
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const cors = initLogicCors(
   Cors({
@@ -12,17 +16,43 @@ const cors = initLogicCors(
  * API WORKS!!!!
  */
 
+async function testSend(a, b, c) {
+  const msg = {
+    to:'malcolm@movement.co.uk',
+    from: 'malcolm.garner4@gmail.com',
+    subject: 'Malcolm testing sengrid api email with form info',
+    text: `Hi Malcolm, mailer api here. ${a} ${b} wishes to contact you. Their email address is ${c} `,
+    html: `<strong>Hi Malcolm, mailer api here. ${a} ${b} wishes to contact you. Their email address is ${c}</strong>`
+  }
+
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  }
+}
 
 export default async function handler(req, res) {
 
   //running the cors
   await cors(req,res)
-
+  
   const data = req.body
-  res.status(200).json({ 
-    Status: "success",
-    FirstName: data.firstName,
-    LastName: data.lastName,
-    Email: data.email
-  })
+
+  const {firstName, lastName, email} = {...data}
+
+  if (firstName && lastName && email) {
+    res.status(200).json({ 
+      Status: "success"
+    })
+    testSend(firstName,lastName,email)
+  }
+
+
+
+  res.end()
 }
