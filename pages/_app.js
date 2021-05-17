aa42d8372d2d11f6b644409c5beb9cca754bdb59
malcolm <Malcom@Malcolms-MacBook-Pro.local>
2021-05-17 22:17:17 +0100

    made child pages with consistent banner

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,14 @@
 import '../styles/global.css'
+import React from "react";
+
 export default function App({Component, pageProps}) {
-    return <Component{...pageProps}/>
+    const Layout = Component.Layout ? Component.Layout : React.Fragment;
+
+
+    return (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )
+
 }
--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,16 +1,12 @@
-import React from 'react'
-import '../components/style_modules/footer.module.css'
-import { options } from 'colorette';
 import innerContent from '../components/style_modules/content.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
-import { JSXElement } from '@babel/types';
-import Layout, { siteTitle } from '../components/layout'
+import Layout from '../components/layout'
 
 
 export default function Aboutme(props): JSX.Element {
     return(
-        <Layout>
+        <>
           <section className={utilStyles.headingMd + ' ' + innerContent.container} >
           <p>test w typescript</p>
           <p>
@@ -18,6 +14,8 @@ export default function Aboutme(props): JSX.Element {
             <a href="https://github.com/Fantominium">here</a>.
           </p>
           </section>
-        </Layout>
+        </>
     )
 }
+
+Aboutme.Layout = Layout;
